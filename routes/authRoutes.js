ae0614a1cc62eb76602c3fa93eec5fafaa41b2d0
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-01-31 21:53:35 +0300

    auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,11 @@
 const { Router } = require("express");
-const { authController } = require("../controllers/authController");
+const authController = require("../controllers/authController");
 
 const router = Router();
 
-router
-  .route("signup")
-  .get("/signup", () => authController.signup_get)
-  .post("/signup", () => authController.signup_post);
-router
-  .route("login")
-  .get("/login", () => authController.login_get)
-  .post("/login", () => authController.login_post);
+router.get("/signup", authController.signup_get);
+router.post("/signup", authController.signup_post);
+router.get("/login", authController.login_get);
+router.post("/login", authController.login_post);
 
 module.exports = router;
