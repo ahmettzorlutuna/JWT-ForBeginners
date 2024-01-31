af30e93f37e6743ce4af46bb2c8617dc6253b42e
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-01-31 21:54:42 +0300

    User creating. And handled error.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,42 @@
-module.exports.signup_get((req,res) => {
-    res.render()
-})
+const User = require("../models/User");
 
-module.exports.signup_post((req,res) => {
-    res.send('Signup success')
-})
+const handleError = (error) => {
+  console.log(error._message, error.code);
+  let errors = { email: "", password: "" };
+  if (error.code === 11000) {
+    errors.email = "The email is already registered. Try another one.";
+    return errors;
+  }
 
-module.exports.login_get((req,res) => {
-    res.render()
-})
+  if (error._message === "user validation failed") {
+    Object.values(error.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
 
-module.exports.login_post((req,res) => {
-    res.send('Login success')
-})
+  return errors;
+};
 
+module.exports.signup_get = (req, res) => {
+  res.render("signup");
+};
 
+module.exports.login_get = (req, res) => {
+  res.render("login");
+};
 
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (err) {
+    const  errors  = handleError(err);
+    res.status(400).json({ errors });
+  }
+};
+
+module.exports.login_post = async (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+};
