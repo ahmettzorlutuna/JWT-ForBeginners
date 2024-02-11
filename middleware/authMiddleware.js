b2d4b9ca76fa331c21a4eebfd5cb1ffbce7e3114
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-02-12 00:00:16 +0300

    checkUser middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,23 @@
+//auth Middleware for verify to auth Token with coming from client.
+const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const verifyAuth = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const token = req.cookies.jwt; //We took the current jwt token from browser
 
   if (token) {
     jwt.verify(token, `${process.env.JWT_SIGN_SECRET}`, (err, decodedToken) => {
-      if (err) {
+      //verify token from browser and our secret key.
+      try {
+        console.log(decodedToken);
+        console.log(res);
+        next(); //if there is no error and verify process is done next executed and page rendered
+      } catch (err) {
         console.log(err.message);
         res.redirect("/auth/login");
-      } else {
-        console.log(decodedToken);
-        next;
       }
     });
   } else {
@@ -21,4 +25,31 @@ const verifyAuth = (req, res, next) => {
   }
 };
 
-module.exports = { verifyAuth };
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt; //We took the current jwt token from browser
+
+  if (token) {
+    jwt.verify(
+      token,
+      `${process.env.JWT_SIGN_SECRET}`,
+      async (err, decodedToken) => {
+        //verify token from browser and our secret key.
+        try {
+          console.log(decodedToken);
+          let user = await User.findById(decodedToken.id);
+          res.locals.user = user;
+          next(); //if there is no error and verify process is done next executed and page rendered
+        } catch (err) {
+          console.log(err.message);
+          res.locals.user = null;
+          next();
+        }
+      }
+    );
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { verifyAuth, checkUser }; //exported
